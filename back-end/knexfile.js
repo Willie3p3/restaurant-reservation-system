7602c11ab98b430376c2c64b7e191d50c83fd0c3
Willie Parker <124847185+Willie3p3@users.noreply.github.com>
2023-11-28 19:14:07 -0500

    Update knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://gytvvrxv:va1Mc_o_p9DjXfNRNoy54hhCFTLrOQ09@berry.db.elephantsql.com/gytvvrxv",
+  DATABASE_URL_DEVELOPMENT = "postgres://gytvvrxv:va1Mc_o_p9DjXfNRNoy54hhCFTLrOQ09@berry.db.elephantsql.com/gytvvrxv",
+  DATABASE_URL_TEST = "postgres://gytvvrxv:va1Mc_o_p9DjXfNRNoy54hhCFTLrOQ09@berry.db.elephantsql.com/gytvvrxv",
+  DATABASE_URL_PREVIEW = "postgres://gytvvrxv:va1Mc_o_p9DjXfNRNoy54hhCFTLrOQ09@berry.db.elephantsql.com/gytvvrxv",
   DEBUG,
 } = process.env;
 
